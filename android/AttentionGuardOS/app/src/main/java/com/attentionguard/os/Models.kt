87ac90689122b36873a488c89432f02f4bd5b0e6
alakhag <alakh1997@gmail.com>
2026-07-07 22:10:49 -0500

    update Wear OS complication

--- android/AttentionGuardOS/app/src/main/java/com/attentionguard/os/Models.kt
+++
@@ -1,3 +0,0 @@
-package com.attentionguard.os
-data class NotificationEvent(val packageName:String,val appName:String,val title:String?,val body:String?,val timestampMillis:Long,val notificationKey:String)
-data class AttentionItem(val id:String,val title:String,val body:String,val source:String,val category:String,val urgency:String,val reason:String,val recommendedAction:String)
